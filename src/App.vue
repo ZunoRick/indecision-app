235c3e509d0237fde4241b48bf962bc573af42ff
ZunoRick <zunosan.ricardo506@gmail.com>
2025-03-10 17:29:48 -0600

    Primera práctica con el proyecto

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <hr />
+  <MyCounter :value="5" />
+  <CounterScript :value="8" />
+  <CounterScript2 :value="2" />
+</template>
+
+<script lang="ts" setup>
+import MyCounter from './components/Counter.vue';
+import CounterScript from './components/CounterScript.vue';
+import CounterScript2 from './components/my-counter-script/CounterScript2.vue';
+</script>
---
+++ src/components/Counter.vue
@@ -0,0 +1,39 @@
+<template>
+  <section>
+    <h3>Counter: {{ counter }}</h3>
+    <h3>Square: {{ squareCounter }}</h3>
+
+    <div>
+      <button class="btn" @click="counter++">+1</button>
+      <button class="btn" @click="counter--">-1</button>
+    </div>
+  </section>
+</template>
+
+<script lang="ts" setup>
+import { useCounter } from '@/composables/useCounter';
+// import { computed, ref } from 'vue';
+
+interface Props {
+  value?: number;
+}
+
+const props = defineProps({
+  value: { type: Number, required: true },
+});
+
+const { counter, squareCounter } = useCounter(props.value);
+
+// const props = defineProps<Props>();
+
+// const counter = ref(props.value ?? 2);
+
+// const squareCounter = computed(() => counter.value * counter.value);
+</script>
+
+<style scoped>
+@reference "../styles.css";
+.btn {
+  @apply p-5 mr-2 text-zinc-50 bg-blue-500 rounded hover:bg-blue-600;
+}
+</style>
