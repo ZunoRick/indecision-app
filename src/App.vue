8cf11e0679ee171cdef22a6228ca53cba71586e7
ZunoRick <zunosan.ricardo506@gmail.com>
2025-03-10 17:44:29 -0600

    Agregando template de mensajes

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,7 @@
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
-  <hr />
-  <MyCounter :value="5" />
-  <CounterScript :value="8" />
-  <CounterScript2 :value="2" />
+  <IndecisionView />
 </template>
 
 <script lang="ts" setup>
-import MyCounter from './components/Counter.vue';
-import CounterScript from './components/CounterScript.vue';
-import CounterScript2 from './components/my-counter-script/CounterScript2.vue';
+import IndecisionView from '@/views/IndecisionView.vue';
 </script>
